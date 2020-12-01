8f9bd5f1e9812a053f64dda428dcb3daf07ddad4
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-30 21:34:34 -0800

    got iex news api working

--- src/pages/StockShow.jsx
+++ src/pages/StockShow.jsx
@@ -7,11 +7,12 @@ class StockShow extends Component {
   state = {
     stockChartXValues: [],
     stockChartYValues: [],
+    news: [],
   };
 
   componentDidMount() {
     this.fetchStock();
-    console.log(this.props.match.params["ticker"]);
+    this.fetchNews();
   }
 
   color = () => {
@@ -20,6 +21,19 @@ class StockShow extends Component {
     } else {
       return "red";
     }
+  };
+  fetchNews = () => {
+    let ticker = this.props.match.params["ticker"];
+    let API_KEY = "pk_306915c8b8c04bf8bb396ac0e15cd378 ";
+    let API_Call = `https://cloud.iexapis.com/stable/stock/${ticker}/news?token=${API_KEY}`;
+    fetch(API_Call)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          news: data,
+        })
+      );
+    console.log(this.state.news);
   };
 
   fetchStock = () => {
@@ -52,47 +66,84 @@ class StockShow extends Component {
         });
       });
   };
+
   render() {
     let upper = this.props.match.params["ticker"].toUpperCase();
     let recentPrice = this.state.stockChartYValues[0];
     let startingPrice = this.state.stockChartYValues[99];
     return (
-      <div>
-        <Plot
-          data={[
-            {
-              x: this.state.stockChartXValues,
-              y: this.state.stockChartYValues,
-              type: "scatter",
-              mode: "lines+markers",
-              marker: {
-                color: this.color(),
-                opacity: "0",
-              },
-            },
-          ]}
-          //MAIN-SVG IN CSS
-          layout={{
-            width: 350,
-            height: 300,
-            font: {
-              color: "white",
-            },
-            xaxis: {
-              showticklabels: false,
-              ticks: "",
-              showgrid: false,
-            },
-            yaxis: {
-              showgrid: false,
-            },
-            paper_bgcolor: "rgba(31, 63, 99, 0)",
-            plot_bgcolor: "rgba(31, 63, 99, 0)",
-            title: `<b>${upper}</b> \n ${recentPrice}`,
-          }}
-        />
-        hi
-      </div>
+      <>
+        <div className="row">
+          <div className="col-10">
+            <Plot
+              data={[
+                {
+                  x: this.state.stockChartXValues,
+                  y: this.state.stockChartYValues,
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: {
+                    color: this.color(),
+                    opacity: "0",
+                  },
+                },
+              ]}
+              //MAIN-SVG IN CSS
+              layout={{
+                width: 1150,
+                height: 400,
+                font: {
+                  color: "white",
+                },
+                xaxis: {
+                  showticklabels: false,
+                  ticks: "",
+                  showgrid: false,
+                },
+                yaxis: {
+                  showgrid: false,
+                },
+                paper_bgcolor: "rgba(31, 63, 99, 0)",
+                plot_bgcolor: "rgba(31, 63, 99, 0)",
+                title: `<b>${upper}</b> \n ${recentPrice}`,
+              }}
+            />
+          </div>
+          <div className="col d-flex flex-column showinfo align-items-center">
+            <div className="cardcontent align-items-center">
+              <div className="p-2">Apple Technology</div>
+              <div className="p-2">
+                125.63 <span>+3.45 (2.43%)</span>
+              </div>
+              <div className="p-1 btn btn-primary">Buy AAPL</div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            {" "}
+            Key Information
+            <div className="row">
+              <div className="col top">Market Cap</div>
+              <div className="col top">PE Ratio</div>
+              <div className="col top">52 week High</div>
+            </div>
+            <div className="row">
+              <div className="col">52 week Low</div>
+              <div className="col">YTD Change</div>
+              <div className="col">Volume</div>
+            </div>
+          </div>
+          <div className="col">
+            News
+            <p>
+              {this.state.news[0]
+                ? this.state.news[0]["headline"]
+                : "no news to display"}
+            </p>
+          </div>
+        </div>
+      </>
     );
   }
 }
