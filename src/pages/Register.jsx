298a7708a26883d1e8c7010a9eea75853391b824
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-12-01 14:34:20 -0800

    register page styled

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,45 +14,62 @@ class Register extends Component {
       password: "",
     };
   }
+
+  //CREATE FUNCTIION HANDLESUBMIT THAT CREATES A NEW USER WHEN REGISTER FORM IS FILLED OUT
   render() {
     return (
       <div>
         <MuiThemeProvider>
-          <div>
-            <TextField
-              floatingLabelText="First Name"
-              onChange={(event, newValue) =>
-                this.setState({ first_name: newValue })
-              }
-            />
-            <br />
-            <TextField
-              floatingLabelText="Last Name"
-              onChange={(event, newValue) =>
-                this.setState({ last_name: newValue })
-              }
-            />
-            <br />
-            <TextField
-              type="email"
-              floatingLabelText="Email"
-              onChange={(event, newValue) => this.setState({ email: newValue })}
-            />
-            <br />
-            <TextField
-              type="password"
-              floatingLabelText="Password"
-              onChange={(event, newValue) =>
-                this.setState({ password: newValue })
-              }
-            />
-            <br />
-            <RaisedButton
-              label="Submit"
-              primary={true}
-              style={style}
-              onClick={(event) => this.handleClick(event)}
-            />
+          <div className="register">
+            <div className="row">
+              <div className="col reg-image-holder">
+                <img
+                  className="reg-image"
+                  src="https://image.flaticon.com/icons/png/512/2037/2037710.png"
+                  alt=""
+                  width="180px"
+                />
+              </div>
+              <div className="col-7 reg-inputs">
+                <h2>Register</h2>
+                <TextField
+                  floatingLabelText="First Name"
+                  onChange={(event, newValue) =>
+                    this.setState({ first_name: newValue })
+                  }
+                />
+                <br />
+                <TextField
+                  floatingLabelText="Last Name"
+                  onChange={(event, newValue) =>
+                    this.setState({ last_name: newValue })
+                  }
+                />
+                <br />
+                <TextField
+                  type="email"
+                  floatingLabelText="Email"
+                  onChange={(event, newValue) =>
+                    this.setState({ email: newValue })
+                  }
+                />
+                <br />
+                <TextField
+                  type="password"
+                  floatingLabelText="Password"
+                  onChange={(event, newValue) =>
+                    this.setState({ password: newValue })
+                  }
+                />
+                <br />
+                <RaisedButton
+                  label="Submit"
+                  primary={true}
+                  style={style}
+                  onClick={(event) => this.handleClick(event)}
+                />
+              </div>
+            </div>
           </div>
         </MuiThemeProvider>
       </div>
